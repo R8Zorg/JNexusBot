6015a45f2a7533be56decab9a868e1beca26c233
Hak1e <hakie.notiko@gmail.com>
2025-07-24 09:45:16 +0400

    Added automatic Hibernate classes registration

--- src/main/java/io/nexusbot/database/HibernateUtil.java
+++ src/main/java/io/nexusbot/database/HibernateUtil.java
@@ -1,25 +1,43 @@
 package io.nexusbot.database;
 
-import io.nexusbot.database.entities.GuildEntity;
+import java.util.List;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ScanResult;
+import jakarta.persistence.Entity;
+
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
+    private static String packagesPath;
 
-    private HibernateUtil() {
+    private HibernateUtil(String packagesPath) {
+        HibernateUtil.packagesPath = packagesPath;
     }
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
                 Configuration configuration = new Configuration().configure();
-                configuration.addAnnotatedClass(GuildEntity.class);
-                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties());
-                sessionFactory = configuration.buildSessionFactory(builder.build());
+                try (ScanResult scanResult = new ClassGraph()
+                        .acceptPackages(packagesPath)
+                        .enableAnnotationInfo()
+                        .scan()) {
+                    List<ClassInfo> classInfos = scanResult.getClassesWithAnnotation(Entity.class);
+                    for (ClassInfo classInfo : classInfos) {
+                        configuration.addAnnotatedClass(classInfo.loadClass());
+                    }
+                    StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                            .applySettings(configuration.getProperties());
+                    sessionFactory = configuration.buildSessionFactory(builder.build());
+
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
+                }
             } catch (Exception e) {
                 System.out.println("Исключение! " + e);
             }

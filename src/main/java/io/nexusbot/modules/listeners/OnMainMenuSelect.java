577283ea0f860d68025aeb3c7a9677738ac0f52c
Hak1e <hakie.notiko@gmail.com>
2025-08-06 20:48:35 +0400

    Listener for handle main menu selection

---
+++ src/main/java/io/nexusbot/modules/listeners/OnMainMenuSelect.java
@@ -0,0 +1,49 @@
+package io.nexusbot.modules.listeners;
+
+import java.awt.Color;
+
+import io.github.r8zorg.jdatools.annotations.EventListeners;
+import io.nexusbot.componentsData.GlobalIds;
+import io.nexusbot.componentsData.MainMenu;
+import io.nexusbot.utils.EmbedUtil;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+@EventListeners
+public class OnMainMenuSelect extends ListenerAdapter {
+    private void tempRooms(StringSelectInteractionEvent event) {
+        MessageEmbed embed = new EmbedBuilder()
+                .setTitle("Временные голосовые комнаты (каналы)")
+                .setDescription("""
+                        Для создания канала-создателя пропишите `/setup rooms`
+                        - Выберите канал, в который нужно зайти, чтобы создать новую комнату.
+                        - Выберите категорию, в которой будет создана новая комната.
+                        """)
+            .setFooter("Это описание может быть неактуальным. Отдавайте предпочтение описанию параметров команды.")
+                .setColor(Color.CYAN)
+                .build();
+        event.replyEmbeds(embed).setEphemeral(true).queue();
+    }
+
+    private void tickets(StringSelectInteractionEvent event) {
+        EmbedUtil.replyEmbed(event, "Настройка билетов в разработке", Color.CYAN);
+
+    }
+
+    @Override
+    public void onStringSelectInteraction(StringSelectInteractionEvent event) {
+        if (!event.getComponentId().equals(MainMenu.id)) {
+            return;
+        }
+        String selectedOptionId = event.getSelectedOptions().get(0).getValue();
+        if (selectedOptionId.equals(GlobalIds.nothing)) {
+            event.deferEdit().queue();
+        } else if (selectedOptionId.equals(MainMenu.tempRooms)) {
+            tempRooms(event);
+        } else if (selectedOptionId.equals(MainMenu.tickets)) {
+            tickets(event);
+        }
+    }
+}

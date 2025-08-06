224db1716132678385bf7e3973744e604933f1f2
Hak1e <hakie.notiko@gmail.com>
2025-08-06 20:44:30 +0400

    Added setup main command

---
+++ src/main/java/io/nexusbot/modules/commands/setup/Setup.java
@@ -0,0 +1,12 @@
+package io.nexusbot.modules.commands.setup;
+
+import io.github.r8zorg.jdatools.annotations.Command;
+import io.github.r8zorg.jdatools.annotations.SlashCommands;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+@SlashCommands
+public class Setup {
+    @Command
+    public void setup(SlashCommandInteractionEvent event) {
+    }
+}

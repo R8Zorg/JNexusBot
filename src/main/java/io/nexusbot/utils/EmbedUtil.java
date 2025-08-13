d8bf820672eb33a47ea45eb6c5607137fcb23c7f
Hak1e <hakie.notiko@gmail.com>
2025-08-13 23:11:01 +0400

    Added sendEmbed in message channel

--- src/main/java/io/nexusbot/utils/EmbedUtil.java
+++ src/main/java/io/nexusbot/utils/EmbedUtil.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
@@ -40,5 +41,13 @@ public class EmbedUtil {
                 .setColor(color)
                 .build();
         event.replyEmbeds(embed).setEphemeral(true).queue();
+    }
+
+    public static void sendEmbed(MessageChannel channel, String description, Color color) {
+        MessageEmbed embed = new EmbedBuilder()
+                .setDescription(description)
+                .setColor(color)
+                .build();
+        channel.sendMessageEmbeds(embed).queue();
     }
 }

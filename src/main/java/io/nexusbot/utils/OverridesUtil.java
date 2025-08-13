7585ee57bce02ba62021d353d301906d88ada9c8
Hak1e <hakie.notiko@gmail.com>
2025-08-14 00:37:03 +0400

    Restored old method adding permissions

--- src/main/java/io/nexusbot/utils/OverridesUtil.java
+++ src/main/java/io/nexusbot/utils/OverridesUtil.java
@@ -3,6 +3,9 @@ package io.nexusbot.utils;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.nexusbot.componentsData.ChannelOverrides;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.PermissionOverride;
@@ -12,6 +15,7 @@ import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.requests.restaction.ChannelAction;
 
 public class OverridesUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OverridesUtil.class);
     public static List<ChannelOverrides> serrializeOverrides(List<PermissionOverride> overrides) {
         List<ChannelOverrides> overwrites = new ArrayList<>();
         overrides.forEach(po -> {
@@ -25,22 +29,31 @@ public class OverridesUtil {
         return overwrites;
     }
 
-    public static void updateChannelOverrides(ChannelAction<VoiceChannel> voiceChannelAction, List<ChannelOverrides> overrides) {
-        for (ChannelOverrides permissionOverwrite : overrides) {
+    public static void updateChannelOverrides(VoiceChannel voiceChannel, List<ChannelOverrides> overwrites) {
+        if (overwrites == null || overwrites.isEmpty()) {
+            return;
+        }
+        for (ChannelOverrides permissionOverwrite : overwrites) {
             String id = permissionOverwrite.getId();
             String type = permissionOverwrite.getType();
             long allow = permissionOverwrite.getAllow();
             long deny = permissionOverwrite.getDeny();
 
             if (type.equals("role")) {
-                Role role = voiceChannelAction.getGuild().getRoleById(id);
+                Role role = voiceChannel.getGuild().getRoleById(id);
                 if (role != null) {
-                    voiceChannelAction.addRolePermissionOverride(role.getIdLong(), allow, deny);
+                    voiceChannel.upsertPermissionOverride(role)
+                            .setAllowed(allow)
+                            .setDenied(deny)
+                            .queue(_ -> {}, error -> LOGGER.info("Error while upserting role permissions: {}", error.getMessage()));
                 }
             } else if (type.equals("member")) {
-                Member member = voiceChannelAction.getGuild().getMemberById(id);
+                Member member = voiceChannel.getGuild().getMemberById(id);
                 if (member != null) {
-                    voiceChannelAction.addMemberPermissionOverride(member.getIdLong(), allow, deny);
+                    voiceChannel.upsertPermissionOverride(member)
+                            .setAllowed(allow)
+                            .setDenied(deny)
+                            .queue(_ -> {}, error -> LOGGER.info("Error while upserting member permissions: {}", error.getMessage()));
                 }
             }
         }

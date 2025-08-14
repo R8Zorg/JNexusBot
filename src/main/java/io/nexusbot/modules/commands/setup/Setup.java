d461af541130cf4359a8bb22203d1331fab1a10f
Hak1e <hakie.notiko@gmail.com>
2025-08-14 15:46:22 +0400

    Added Permission.ADMINISTRATOR

--- src/main/java/io/nexusbot/modules/commands/setup/Setup.java
+++ src/main/java/io/nexusbot/modules/commands/setup/Setup.java
@@ -1,12 +1,16 @@
 package io.nexusbot.modules.commands.setup;
 
+import io.github.r8zorg.jdatools.annotations.AdditionalSettings;
 import io.github.r8zorg.jdatools.annotations.Command;
 import io.github.r8zorg.jdatools.annotations.SlashCommands;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 @SlashCommands
 public class Setup {
     @Command
+    @AdditionalSettings(defaultPermissions = Permission.ADMINISTRATOR)
     public void setup(SlashCommandInteractionEvent event) {
     }
+
 }

b2b2c9d53b60f18e1480d398c53432a108c05707
Hak1e <hakie.notiko@gmail.com>
2025-08-26 00:37:47 +0400

    Added Enum for TempRoomSettingsModal

---
+++ src/main/java/io/nexusbot/componentsData/TempRoomSettingsModal.java
@@ -0,0 +1,20 @@
+package io.nexusbot.componentsData;
+
+public enum TempRoomSettingsModal {
+    STATUS(TempRoomSettingsMenu.STATUS.getValue() + GlobalIds.BASE_MODAL_ID.getValue()),
+    LIMIT(TempRoomSettingsMenu.LIMIT.getValue() + GlobalIds.BASE_MODAL_ID.getValue()),
+    BITRATE(TempRoomSettingsMenu.BITRATE.getValue() + GlobalIds.BASE_MODAL_ID.getValue()),
+    NAME(TempRoomSettingsMenu.NAME.getValue() + GlobalIds.BASE_MODAL_ID.getValue());
+
+    public static final String ID = "temp-room-settings" + GlobalIds.BASE_MODAL_ID.getValue();
+
+    private final String value;
+
+    TempRoomSettingsModal(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}

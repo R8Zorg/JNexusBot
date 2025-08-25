7748d15048573ea09e23d546cb1c73745d320916
Hak1e <hakie.notiko@gmail.com>
2025-08-25 12:07:12 +0400

    Added ids for temp room select menus

---
+++ src/main/java/io/nexusbot/componentsData/RoomPermissions.java
@@ -0,0 +1,15 @@
+package io.nexusbot.componentsData;
+
+public class RoomPermissions {
+    public static final String id = "room-permissions";
+    public static final String lock = "room-lock";
+    public static final String unlock = "room-unlock";
+    public static final String permit = "room-permit";
+    public static final String reject = "room-reject";
+    public static final String kick = "room-kick";
+
+    public static final String ghost = "room-ghost";
+    public static final String unghost = "room-unghost";
+    public static final String accept = "room-whitelist-add";
+    public static final String deny = "room-whitelist-remove";
+}
---
+++ src/main/java/io/nexusbot/componentsData/RoomSettings.java
@@ -0,0 +1,13 @@
+package io.nexusbot.componentsData;
+
+public class RoomSettings {
+    public static final String id = "room-settings";
+    public static final String limit = "room-limit";
+    public static final String status = "room-status";
+    public static final String bitrate = "room-bitrate";
+    public static final String nsfw = "room-nsfw";
+    public static final String claim = "room-claim";
+
+    public static final String name = "room-name";
+
+}

fbcde6943eafe7fcf4d41ec3988db082e19423f4
Hak1e <hakie.notiko@gmail.com>
2025-08-25 10:27:09 +0400

    Added update info message method

--- src/main/java/io/nexusbot/utils/MessageActionUtil.java
+++ src/main/java/io/nexusbot/utils/MessageActionUtil.java
@@ -1,9 +1,19 @@
 package io.nexusbot.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.nexusbot.database.entities.TempRoom;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.MessageEmbed.Thumbnail;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 
 public class MessageActionUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageActionUtil.class);
+
     public static void deleteInfoMessage(Guild guild, Long channelId, Long messageId) {
         if (channelId == null || messageId == null) {
             return;
@@ -12,5 +22,53 @@ public class MessageActionUtil {
         if (infoChannel != null) {
             infoChannel.deleteMessageById(messageId).queue();
         }
+    }
+
+    private static MessageEmbed getUpdatedEmbed(MessageEmbed embed, VoiceChannel room) {
+        int membersCount = room.getMembers().size();
+        String membersFieldValue = "";
+        for (int i = 0; i < membersCount; i++) {
+            membersFieldValue += "【" + (i + 1) + "】"
+                    + room.getMembers().get(i).getAsMention()
+                    + "\n";
+        }
+
+        String channelLinkFieldValue = "";
+        if (room.getUserLimit() != 0 && membersCount >= room.getUserLimit()) {
+            channelLinkFieldValue = "\n**❌ Канал заполнен**";
+        } else {
+            channelLinkFieldValue = "\n**✅ Канал:** " + room.getAsMention();
+        }
+
+        String userLimit = room.getUserLimit() == 0 ? "∞" : String.valueOf(room.getUserLimit());
+        EmbedBuilder updatedEmbed = new EmbedBuilder()
+                .setTitle(embed.getTitle())
+                .setColor(embed.getColor())
+                .addField("", membersFieldValue, false)
+                .addField("", channelLinkFieldValue, false)
+                .setFooter("Участников: " + membersCount + "/" + userLimit);
+
+        Thumbnail thumbnail = embed.getThumbnail();
+        if (thumbnail != null && thumbnail.getUrl() != null) {
+            updatedEmbed.setThumbnail(thumbnail.getUrl());
+        }
+
+        return updatedEmbed.build();
+    }
+
+    public static void updateInfoMessage(Guild guild, Long channelId, Long messageId, TempRoom tempRoom,
+            VoiceChannel voiceChannel) {
+        TextChannel channel = guild.getChannelById(TextChannel.class, channelId);
+        if (channel == null) {
+            return;
+        }
+        channel.retrieveMessageById(messageId).queue(infoMessage -> {
+            MessageEmbed updatedEmbed = getUpdatedEmbed(infoMessage.getEmbeds().get(0), voiceChannel);
+            infoMessage.editMessageEmbeds(updatedEmbed).queue(_ -> {
+            }, errorResponse -> {
+                LOGGER.warn("Не удалось обновить сообщение - нет доступа. Ошибка: {}",
+                        errorResponse.getMessage());
+            });
+        });
     }
 }

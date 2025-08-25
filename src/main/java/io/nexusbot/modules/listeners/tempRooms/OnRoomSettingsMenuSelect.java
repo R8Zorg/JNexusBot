7b4b154bff0f196e4e2b0fcef791bb72915e1d89
Hak1e <hakie.notiko@gmail.com>
2025-08-26 00:40:44 +0400

    Added settings dropdown menu

---
+++ src/main/java/io/nexusbot/modules/listeners/tempRooms/OnRoomSettingsMenuSelect.java
@@ -0,0 +1,83 @@
+package io.nexusbot.modules.listeners.tempRooms;
+
+import java.awt.Color;
+
+import io.github.r8zorg.jdatools.annotations.EventListeners;
+import io.nexusbot.componentsData.GlobalIds;
+import io.nexusbot.componentsData.TempRoomSettingsMenu;
+import io.nexusbot.componentsData.TempRoomSettingsModal;
+import io.nexusbot.utils.EmbedUtil;
+import io.nexusbot.utils.ModalUtil;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+@EventListeners
+public class OnRoomSettingsMenuSelect extends ListenerAdapter {
+
+    private void setStatus(StringSelectInteractionEvent event) {
+        event.replyModal(ModalUtil.generateModal(
+                TempRoomSettingsModal.STATUS.getValue(), "Изменить статус канала",
+                "Введите новый статус", "Статус канала", 1, 128))
+                .queue();
+    }
+
+    private void setLimit(StringSelectInteractionEvent event) {
+        event.replyModal(ModalUtil.generateModal(
+                TempRoomSettingsModal.LIMIT.getValue(), "Изменить лимит участников",
+                "Введите новый лимит", "0 - 99", 1, 2))
+                .queue();
+    }
+
+    private void setBitrate(StringSelectInteractionEvent event) {
+        event.replyModal(ModalUtil.generateModal(
+                TempRoomSettingsModal.BITRATE.getValue(), "Изменить битрейт канала",
+                "Введите новый битрейт", "8 - 96", 1, 3))
+                .queue();
+    }
+
+    private void setNsfw(StringSelectInteractionEvent event) {
+        VoiceChannel voiceChannel = event.getChannel().asVoiceChannel();
+        boolean isNsfw = !voiceChannel.isNSFW();
+        voiceChannel.getManager().setNSFW(isNsfw).queue();
+        if (isNsfw) {
+            EmbedUtil.replyEmbed(event, "Каналу выставлено ограничение 18+", Color.GREEN);
+        } else {
+            EmbedUtil.replyEmbed(event, "С канала снято ограничение 18+", Color.GREEN);
+        }
+    }
+
+    private void setOwnership(StringSelectInteractionEvent event) {
+        EmbedUtil.replyEmbed(event, "Эта функция ещё в разработке", Color.ORANGE);
+    }
+
+    private void setName(StringSelectInteractionEvent event) {
+        event.replyModal(ModalUtil.generateModal(
+                TempRoomSettingsModal.NAME.getValue(), "Изменить название канала",
+                "Введите новое название", "название канала", 1, 128))
+                .queue();
+    }
+
+    @Override
+    public void onStringSelectInteraction(StringSelectInteractionEvent event) {
+        if (!event.getComponentId().equals(TempRoomSettingsMenu.ID)) {
+            return;
+        }
+
+        String selectedOptionId = event.getSelectedOptions().get(0).getValue();
+        if (selectedOptionId.equals(GlobalIds.NOTHING.getValue())){
+            event.deferEdit().queue();
+            return;
+        }
+
+        switch (TempRoomSettingsMenu.fromValue(selectedOptionId)) {
+            case STATUS -> setStatus(event);
+            case LIMIT -> setLimit(event);
+            case BITRATE -> setBitrate(event);
+            case NSFW -> setNsfw(event);
+            case CLAIM -> setOwnership(event);
+            case NAME -> setName(event);
+        }
+
+    }
+}

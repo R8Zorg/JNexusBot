5f9b5e643b3527ddfe2ceac7dbf4406db3110254
Hak1e <hakie.notiko@gmail.com>
2025-08-29 15:16:59 +0400

    Overrides saves via TempRoomUtil

--- src/main/java/io/nexusbot/modules/listeners/tempRooms/OnRoomPermissionsMenuSelect.java
+++ src/main/java/io/nexusbot/modules/listeners/tempRooms/OnRoomPermissionsMenuSelect.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import io.github.r8zorg.jdatools.annotations.EventListeners;
-import io.nexusbot.componentsData.ChannelOverrides;
+import io.nexusbot.componentsData.DiscordConstants;
 import io.nexusbot.componentsData.GlobalIds;
 import io.nexusbot.componentsData.TempRoomPermissionsMenu;
 import io.nexusbot.database.entities.TempRoom;
@@ -13,7 +13,7 @@ import io.nexusbot.database.entities.TempRoomSettings;
 import io.nexusbot.database.services.TempRoomService;
 import io.nexusbot.database.services.TempRoomSettingsService;
 import io.nexusbot.utils.EmbedUtil;
-import io.nexusbot.utils.OverridesUtil;
+import io.nexusbot.utils.TempRoomUtil;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
@@ -30,33 +30,27 @@ public class OnRoomPermissionsMenuSelect extends ListenerAdapter {
     private TempRoomService tempRoomService = new TempRoomService();
     private TempRoomSettingsService tempRoomSettingsService = new TempRoomSettingsService();
 
-    private void updateAndSaveRoomOverrides(VoiceChannel voiceChannel, TempRoomSettings tempRoomSettings) {
-        List<ChannelOverrides> overrides = OverridesUtil.serrializeOverrides(voiceChannel.getPermissionOverrides());
-        tempRoomSettings.setOverrides(overrides);
-        tempRoomSettingsService.saveOrUpdate(tempRoomSettings);
-    }
-
-    private void lockRoom(StringSelectInteractionEvent event, TempRoomSettings tempRoomSettings) {
+    private void lockRoom(StringSelectInteractionEvent event) {
         VoiceChannel room = event.getChannel().asVoiceChannel();
         room.upsertPermissionOverride(event.getGuild().getPublicRole())
                 .setDenied(Permission.VOICE_CONNECT).queue(_ -> {
-                    updateAndSaveRoomOverrides(room, tempRoomSettings);
+                    TempRoomUtil.saveOverrides(room, event.getMember().getIdLong());
                     EmbedUtil.replyEmbed(event, "Канал закрыт", Color.WHITE);
                 });
     }
 
-    private void unlockRoom(StringSelectInteractionEvent event, TempRoomSettings tempRoomSettings) {
+    private void unlockRoom(StringSelectInteractionEvent event) {
         VoiceChannel room = event.getChannel().asVoiceChannel();
         room.upsertPermissionOverride(event.getGuild().getPublicRole())
                 .clear(Permission.VOICE_CONNECT).queue(_ -> {
-                    updateAndSaveRoomOverrides(room, tempRoomSettings);
+                    TempRoomUtil.saveOverrides(room, event.getMember().getIdLong());
                     EmbedUtil.replyEmbed(event, "Канал открыт", Color.WHITE);
                 });
     }
 
     private void rejectMember(StringSelectInteractionEvent event) {
         MessageEmbed embed = EmbedUtil.generateEmbed("""
-                Выберите участника, которому хотите запретить вход в канал.
+                Выберите участников, которому хотите запретить вход в канал.
                 Если участника нет в списке, воспользуйтесь слеш командной `/room reject`
                 """, Color.WHITE);
 
@@ -64,18 +58,21 @@ public class OnRoomPermissionsMenuSelect extends ListenerAdapter {
                 .addActionRow(EntitySelectMenu.create(
                         TempRoomPermissionsMenu.REJECT.getValue(), SelectTarget.USER)
                         .setPlaceholder("Выберите участника")
+                        .setMaxValues(DiscordConstants.MAX_SELECT_MENU_ITEMS.getValue())
                         .build())
                 .setEphemeral(true)
                 .queue();
     }
 
     private StringSelectMenu getMembersMenu(List<Member> members, String menuId) {
+        int membersAmount = DiscordConstants.MAX_SELECT_MENU_ITEMS.getValue() - 1;
         Builder selectMenuBuilder = StringSelectMenu.create(menuId)
-                .setPlaceholder("Выберите участника")
+                .setPlaceholder("Выберите участников")
+                .setMaxValues(membersAmount)
                 .addOption("Никого не выбирать", GlobalIds.NOTHING.getValue());
         for (Member member : members) {
             selectMenuBuilder.addOption(member.getEffectiveName(), member.getId());
-            if (selectMenuBuilder.getOptions().size() >= 25) {
+            if (selectMenuBuilder.getOptions().size() >= membersAmount) {
                 break;
             }
         }
@@ -84,7 +81,7 @@ public class OnRoomPermissionsMenuSelect extends ListenerAdapter {
 
     private void permitMember(StringSelectInteractionEvent event) {
         MessageEmbed embed = EmbedUtil.generateEmbed("""
-                Выберите участника, с которого хотите снять запрет на вход в канал.
+                Выберите участников, с которого хотите снять запрет на вход в канал.
                 """, Color.WHITE);
         List<Member> rejectedMembers = event.getChannel().asVoiceChannel().getPermissionOverrides()
                 .stream()
@@ -105,7 +102,7 @@ public class OnRoomPermissionsMenuSelect extends ListenerAdapter {
 
     private void kickMember(StringSelectInteractionEvent event) {
         MessageEmbed embed = EmbedUtil.generateEmbed("""
-                Выберите участника, которого хотите выгнать из канала.
+                Выберите участников, которого хотите выгнать из канала.
                 Вы также можете использовать слеш команду `/room disconnect`
                 """, Color.WHITE);
 
@@ -116,33 +113,34 @@ public class OnRoomPermissionsMenuSelect extends ListenerAdapter {
                 .queue();
     }
 
-    private void ghostRoom(StringSelectInteractionEvent event, TempRoomSettings tempRoomSettings) {
+    private void ghostRoom(StringSelectInteractionEvent event) {
         VoiceChannel room = event.getChannel().asVoiceChannel();
         room.upsertPermissionOverride(event.getGuild().getPublicRole())
                 .setDenied(Permission.VIEW_CHANNEL).queue(_ -> {
-                    updateAndSaveRoomOverrides(room, tempRoomSettings);
+                    TempRoomUtil.saveOverrides(room, event.getMember().getIdLong());
                     EmbedUtil.replyEmbed(event, "Канал скрыт", Color.WHITE);
                 });
     }
 
-    private void unghostRoom(StringSelectInteractionEvent event, TempRoomSettings tempRoomSettings) {
+    private void unghostRoom(StringSelectInteractionEvent event) {
         VoiceChannel room = event.getChannel().asVoiceChannel();
         room.upsertPermissionOverride(event.getGuild().getPublicRole())
-                .setAllowed(Permission.VIEW_CHANNEL).queue(_ -> {
-                updateAndSaveRoomOverrides(room, tempRoomSettings);
-                EmbedUtil.replyEmbed(event, "Канал явлен", Color.WHITE);
-            });
+                .clear(Permission.VIEW_CHANNEL).queue(_ -> {
+                    TempRoomUtil.saveOverrides(room, event.getMember().getIdLong());
+                    EmbedUtil.replyEmbed(event, "Канал явлен", Color.WHITE);
+                });
     }
 
     private void acceptMember(StringSelectInteractionEvent event) {
         MessageEmbed embed = EmbedUtil.generateEmbed("""
-                Выберите участника, который будет видеть скрытый канал.
+                Выберите участников, который будет видеть скрытый канал.
                 Если участника нет в списке, воспользуйтесь слеш командной `/room accept`
                 """, Color.WHITE);
 
         event.replyEmbeds(embed)
                 .addActionRow(EntitySelectMenu.create(
                         TempRoomPermissionsMenu.ACCEPT.getValue(), SelectTarget.USER)
+                        .setMaxValues(DiscordConstants.MAX_SELECT_MENU_ITEMS.getValue())
                         .setPlaceholder("Выберите участника")
                         .build())
                 .setEphemeral(true)
@@ -151,7 +149,7 @@ public class OnRoomPermissionsMenuSelect extends ListenerAdapter {
 
     private void denyMember(StringSelectInteractionEvent event) {
         MessageEmbed embed = EmbedUtil.generateEmbed("""
-                Выберите участника, которому хотите сброить доступ к закрытому каналу.
+                Выберите участников, которому хотите сброить доступ к закрытому каналу.
                 """, Color.WHITE);
         List<Member> acceptedMembers = event.getChannel().asVoiceChannel().getPermissionOverrides()
                 .stream()
@@ -194,13 +192,13 @@ public class OnRoomPermissionsMenuSelect extends ListenerAdapter {
 
         TempRoomSettings tempRoomSettings = tempRoomSettingsService.get(ownerId, event.getGuild().getIdLong());
         switch (TempRoomPermissionsMenu.fromValue(selectedOptionId)) {
-            case LOCK -> lockRoom(event, tempRoomSettings);
-            case UNLOCK -> unlockRoom(event, tempRoomSettings);
+            case LOCK -> lockRoom(event);
+            case UNLOCK -> unlockRoom(event);
             case REJECT -> rejectMember(event);
             case PERMIT -> permitMember(event);
             case KICK -> kickMember(event);
-            case GHOST -> ghostRoom(event, tempRoomSettings);
-            case UNGHOST -> unghostRoom(event, tempRoomSettings);
+            case GHOST -> ghostRoom(event);
+            case UNGHOST -> unghostRoom(event);
             case ACCEPT -> acceptMember(event);
             case DENY -> denyMember(event);
         }

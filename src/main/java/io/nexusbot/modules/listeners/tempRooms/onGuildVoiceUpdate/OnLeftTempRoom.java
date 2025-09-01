2effba11319e11747699b07328706f526cf81a13
Hak1e <hakie.notiko@gmail.com>
2025-09-01 12:13:22 +0400

    Removed save settings before deleting channel

--- src/main/java/io/nexusbot/modules/listeners/tempRooms/onGuildVoiceUpdate/OnLeftTempRoom.java
+++ src/main/java/io/nexusbot/modules/listeners/tempRooms/onGuildVoiceUpdate/OnLeftTempRoom.java
@@ -1,15 +1,9 @@
 package io.nexusbot.modules.listeners.tempRooms.onGuildVoiceUpdate;
 
-import java.util.List;
-
 import io.github.r8zorg.jdatools.annotations.EventListeners;
-import io.nexusbot.componentsData.ChannelOverrides;
 import io.nexusbot.database.entities.TempRoom;
-import io.nexusbot.database.entities.TempRoomSettings;
 import io.nexusbot.database.services.TempRoomService;
-import io.nexusbot.database.services.TempRoomSettingsService;
 import io.nexusbot.utils.MessageActionUtil;
-import io.nexusbot.utils.OverridesUtil;
 import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -17,20 +11,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 @EventListeners
 public class OnLeftTempRoom extends ListenerAdapter {
     private TempRoomService tempRoomService = new TempRoomService();
-    private TempRoomSettingsService settingsService = new TempRoomSettingsService();
-
-    public void saveAllRoomSettings(VoiceChannel voiceChannel, long ownerId) {
-        List<ChannelOverrides> overrides = OverridesUtil.serrializeOverrides(voiceChannel.getPermissionOverrides());
-        TempRoomSettings roomSettings = settingsService.getOrCreate(ownerId, voiceChannel.getGuild().getIdLong());
-
-        roomSettings.setOverrides(overrides);
-        roomSettings.setName(voiceChannel.getName());
-        roomSettings.setNsfw(voiceChannel.isNSFW());
-        roomSettings.setStatus(voiceChannel.getStatus());
-        roomSettings.setBitrate(voiceChannel.getBitrate());
-        roomSettings.setUserLimit(voiceChannel.getUserLimit());
-        settingsService.saveOrUpdate(roomSettings);
-    }
 
     @Override
     public void onGuildVoiceUpdate(GuildVoiceUpdateEvent event) {
@@ -45,7 +25,6 @@ public class OnLeftTempRoom extends ListenerAdapter {
         }
 
         if (leftChannel.getMembers().isEmpty()) {
-            saveAllRoomSettings(leftChannel, tempRoom.getOwnerId());
             leftChannel.delete().queue();
             if (tempRoom.getLogMessageId() != null) {
                 MessageActionUtil.deleteInfoMessage(event.getGuild(), tempRoom.getChannelLogId(),

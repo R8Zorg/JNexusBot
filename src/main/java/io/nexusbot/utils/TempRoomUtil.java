45a4ea8facb6a12bf8e1f5d99012693a422a68a4
Hak1e <hakie.notiko@gmail.com>
2025-09-01 12:58:20 +0400

    Added save room settings: name, bitrate, user limit, voice status, nsfw. Code refactored

--- src/main/java/io/nexusbot/utils/TempRoomUtil.java
+++ src/main/java/io/nexusbot/utils/TempRoomUtil.java
@@ -1,6 +1,7 @@
 package io.nexusbot.utils;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 import io.nexusbot.componentsData.ChannelOverrides;
 import io.nexusbot.database.entities.TempRoomSettings;
@@ -10,10 +11,40 @@ import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 public class TempRoomUtil {
     private static TempRoomSettingsService settingsService = new TempRoomSettingsService();
 
-    public static void saveOverrides(VoiceChannel voiceChannel, long ownerId) {
+    private static void saveRoomSettings(VoiceChannel voiceChannel, long ownerId, Consumer<TempRoomSettings> action) {
         TempRoomSettings settings = settingsService.getOrCreate(ownerId, voiceChannel.getGuild().getIdLong());
-        List<ChannelOverrides> overrides = OverridesUtil.serrializeOverrides(voiceChannel.getPermissionOverrides());
-        settings.setOverrides(overrides);
+        action.accept(settings);
         settingsService.saveOrUpdate(settings);
+    }
+
+    public static void saveOverrides(VoiceChannel voiceChannel, long ownerId) {
+        List<ChannelOverrides> overrides = OverridesUtil.serrializeOverrides(voiceChannel.getPermissionOverrides());
+        saveRoomSettings(voiceChannel, ownerId,
+                settings -> settings.setOverrides(overrides));
+    }
+
+    public static void saveBitrate(VoiceChannel voiceChannel, long ownerId) {
+        saveRoomSettings(voiceChannel, ownerId,
+                settings -> settings.setBitrate(voiceChannel.getBitrate()));
+    }
+
+    public static void saveNsfw(VoiceChannel voiceChannel, long ownerId) {
+        saveRoomSettings(voiceChannel, ownerId,
+                settings -> settings.setNsfw(voiceChannel.isNSFW()));
+    }
+
+    public static void saveName(VoiceChannel voiceChannel, long ownerId) {
+        saveRoomSettings(voiceChannel, ownerId,
+                settings -> settings.setName(voiceChannel.getName()));
+    }
+
+    public static void saveUserLimit(VoiceChannel voiceChannel, long ownerId) {
+        saveRoomSettings(voiceChannel, ownerId,
+                settings -> settings.setUserLimit(voiceChannel.getUserLimit()));
+    }
+
+    public static void saveVoiceStatus(VoiceChannel voiceChannel, long ownerId) {
+        saveRoomSettings(voiceChannel, ownerId,
+                settings -> settings.setStatus(voiceChannel.getStatus()));
     }
 }

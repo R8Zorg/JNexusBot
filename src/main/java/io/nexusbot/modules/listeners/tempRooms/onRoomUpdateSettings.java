a6252913f9aadb3aef6fcc23d939f99b37b5fd8f
Hak1e <hakie.notiko@gmail.com>
2025-09-08 12:18:55 +0400

    Work in progress

--- src/main/java/io/nexusbot/modules/listeners/tempRooms/onRoomUpdateSettings.java
+++ src/main/java/io/nexusbot/modules/listeners/tempRooms/onRoomUpdateSettings.java
@@ -1,11 +1,20 @@
 package io.nexusbot.modules.listeners.tempRooms;
 
+import java.util.EnumSet;
+import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.github.r8zorg.jdatools.annotations.EventListeners;
 import io.nexusbot.database.entities.TempRoom;
 import io.nexusbot.database.services.TempRoomService;
 import io.nexusbot.utils.TempRoomUtil;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.PermissionOverride;
 import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.events.channel.update.ChannelUpdateBitrateEvent;
 import net.dv8tion.jda.api.events.channel.update.ChannelUpdateNSFWEvent;
@@ -13,16 +22,30 @@ import net.dv8tion.jda.api.events.channel.update.ChannelUpdateNameEvent;
 import net.dv8tion.jda.api.events.channel.update.ChannelUpdateUserLimitEvent;
 import net.dv8tion.jda.api.events.channel.update.ChannelUpdateVoiceStatusEvent;
 import net.dv8tion.jda.api.events.channel.update.GenericChannelUpdateEvent;
-import net.dv8tion.jda.api.events.guild.override.PermissionOverrideCreateEvent;
+import net.dv8tion.jda.api.events.guild.override.GenericPermissionOverrideEvent;
 import net.dv8tion.jda.api.events.guild.override.PermissionOverrideDeleteEvent;
 import net.dv8tion.jda.api.events.guild.override.PermissionOverrideUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 @EventListeners
 public class onRoomUpdateSettings extends ListenerAdapter {
+    private Logger LOGGER = LoggerFactory.getLogger(onRoomUpdateSettings.class);
     private TempRoomService roomService = new TempRoomService();
+    private final List<Permission> deniedPermissions = List.of(
+            Permission.MANAGE_CHANNEL, Permission.MANAGE_WEBHOOKS,
+            Permission.PRIORITY_SPEAKER, Permission.VOICE_MUTE_OTHERS,
+            Permission.VOICE_DEAF_OTHERS, Permission.MESSAGE_MENTION_EVERYONE,
+            Permission.MESSAGE_TTS, Permission.CREATE_SCHEDULED_EVENTS,
+            Permission.MANAGE_EVENTS);
 
     private void handleChannelUpdate(GenericChannelUpdateEvent<?> event, BiConsumer<VoiceChannel, Long> action) {
+        TempRoom room = roomService.get(event.getChannel().getIdLong());
+        if (room != null) {
+            action.accept(event.getChannel().asVoiceChannel(), room.getOwnerId());
+        }
+    }
+
+    private void handleChannelUpdate(GenericPermissionOverrideEvent event, BiConsumer<VoiceChannel, Long> action) {
         TempRoom room = roomService.get(event.getChannel().getIdLong());
         if (room != null) {
             action.accept(event.getChannel().asVoiceChannel(), room.getOwnerId());
@@ -55,21 +78,23 @@ public class onRoomUpdateSettings extends ListenerAdapter {
     }
 
     @Override
-    public void onPermissionOverrideCreate(PermissionOverrideCreateEvent event) {
-        // TODO Auto-generated method stub
-        super.onPermissionOverrideCreate(event);
+    public void onPermissionOverrideUpdate(PermissionOverrideUpdateEvent event) {
+        // TODO: убрать из списка права, которые есть в deniedPermissions
+        LOGGER.info("Override update");
+        // PermissionOverride permissionOverride = event.getPermissionOverride();
+        // List<Permission> permissions = Stream.concat(
+        //         permissionOverride.getDenied(), permissionOverride.getAllowed())
+        //         .filter(override -> !deniedPermissions.contains(override));
+
+        // permissions.addAll(permissionOverride.getDenied());
+        // permissions.removeAll(deniedPermissions);
+        handleChannelUpdate(event, TempRoomUtil::saveOverrides);
     }
 
     @Override
     public void onPermissionOverrideDelete(PermissionOverrideDeleteEvent event) {
-        // TODO Auto-generated method stub
-        super.onPermissionOverrideDelete(event);
-    }
-
-    @Override
-    public void onPermissionOverrideUpdate(PermissionOverrideUpdateEvent event) {
-        // TODO Auto-generated method stub
-        super.onPermissionOverrideUpdate(event);
+        LOGGER.info("Override delete");
+        handleChannelUpdate(event, TempRoomUtil::saveOverrides);
     }
 
 }

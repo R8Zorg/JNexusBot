80f4b1ad2e55e0c0fba7ad6c69c5f654cf64b528
Hak1e <hakie.notiko@gmail.com>
2025-09-14 01:47:56 +0400

    Added additional settings, ownerOnly annotation

--- src/main/java/io/nexusbot/modules/commands/OwnerCommands.java
+++ src/main/java/io/nexusbot/modules/commands/OwnerCommands.java
@@ -3,6 +3,7 @@ package io.nexusbot.modules.commands;
 import java.awt.Color;
 import java.util.List;
 
+import io.github.r8zorg.jdatools.annotations.AdditionalSettings;
 import io.github.r8zorg.jdatools.annotations.Command;
 import io.github.r8zorg.jdatools.annotations.Option;
 import io.github.r8zorg.jdatools.annotations.OwnerOnly;
@@ -11,6 +12,7 @@ import io.github.r8zorg.jdatools.annotations.Subcommand;
 import io.nexusbot.database.entities.BotOwner;
 import io.nexusbot.database.services.BotOwnerService;
 import io.nexusbot.utils.EmbedUtil;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
@@ -19,55 +21,56 @@ public class OwnerCommands {
     BotOwnerService botOwnerService = new BotOwnerService();
 
     @Command(description = "Owner commands")
-    @OwnerOnly
+    @OwnerOnly(title = "Ошибка доступа", description = "Вы не можете использовать эту команду.", footer = "Эта группа команд доступна только владельцу бота.")
+    @AdditionalSettings(defaultPermissions = Permission.ADMINISTRATOR)
     public void owners(SlashCommandInteractionEvent event) {
     }
 
-    @Subcommand(parentNames = "owners", description = "Get list of bot's owners")
+    @Subcommand(parentNames = "owners", description = "Получить список владельцев бота")
     public void list(SlashCommandInteractionEvent event) {
         try {
             List<BotOwner> owners = botOwnerService.getAll();
             if (owners.isEmpty()) {
-                EmbedUtil.replyEmbed(event, "There are no owners", Color.YELLOW);
+                EmbedUtil.replyEmbed(event, "Владельцев нет.", Color.YELLOW);
                 return;
             }
-            String replyMessage = "My owners:\n";
+            String replyMessage = "Владельцы бота:";
             for (BotOwner botOwner : owners) {
                 replyMessage += "<@" + botOwner.getId() + ">\n";
             }
             EmbedUtil.replyEmbed(event, replyMessage, Color.GREEN);
         } catch (Exception e) {
-            EmbedUtil.replyEmbed(event, "An error occured: " + e.getMessage(), Color.RED);
+            EmbedUtil.replyEmbed(event, "Произошла ошибка: " + e.getMessage(), Color.RED);
         }
     }
 
-    @Subcommand(parentNames = "owners", description = "Add user to bot's owners")
+    @Subcommand(parentNames = "owners", description = "Добавить пользователя в список создателей бота")
     public void add(SlashCommandInteractionEvent event,
-            @Option(name = "user", description = "User to add as bot's owners") User user) {
+            @Option(name = "user", description = "Пользователь") User user) {
         try {
             botOwnerService.add(user.getIdLong());
-            EmbedUtil.replyEmbed(event, user.getAsMention() + " added to owners.", Color.GREEN);
+            EmbedUtil.replyEmbed(event, user.getAsMention() + " добавлен.", Color.GREEN);
         } catch (Exception e) {
-            EmbedUtil.replyEmbed(event, "An error occured while adding user " + user.getName() + " to DB.", Color.RED);
+            EmbedUtil.replyEmbed(event, "Произошла ошибка: " + e.getMessage(), Color.RED);
         }
     }
 
-    @Subcommand(parentNames = "owners", description = "Remove user from bot's owners")
+    @Subcommand(parentNames = "owners", description = "Убрать пользователя из списка владельцев")
     public void remove(SlashCommandInteractionEvent event,
-            @Option(name = "user", description = "User to remove from bot's owners") User user) {
+            @Option(name = "user", description = "Пользователь") User user) {
         if (user.getIdLong() == event.getUser().getIdLong()) {
-            EmbedUtil.replyEmbed(event, "You can't remove yourself from owners.", Color.RED);
+            EmbedUtil.replyEmbed(event, "Вы не можете убрать себя из списка владельцев.", Color.RED);
             return;
         }
         try {
             botOwnerService.remove(user.getIdLong());
-            EmbedUtil.replyEmbed(event, user.getAsMention() + " removed from owners.", Color.GREEN);
+            EmbedUtil.replyEmbed(event, user.getAsMention() + " убран из списка владельцев.", Color.GREEN);
         } catch (IllegalArgumentException e) {
-            EmbedUtil.replyEmbed(event, "Unable to remove " + user.getAsMention() + " because user is not the owner", Color.RED);
-        } 
-        catch (Exception e) {
-            EmbedUtil.replyEmbed(event, "An error occured while removing user " + user.getName() + " from DB: " + e.getMessage(),
+            EmbedUtil.replyEmbed(event, "Пользователь и так не владелец бота",
                     Color.RED);
+        } catch (Exception e) {
+            EmbedUtil.replyEmbed(event,
+                    "Произошла ошибка: " + e.getMessage(), Color.RED);
         }
     }
 }

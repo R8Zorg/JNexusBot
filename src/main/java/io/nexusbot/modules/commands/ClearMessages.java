a9a1e9cec7e3c486469876258d4ba6d7b6dc9e79
R8Zorg <hakie.notiko@gmail.com>
2025-10-03 12:36:34 +0400

    Delete messages extracted to method

--- src/main/java/io/nexusbot/modules/commands/ClearMessages.java
+++ src/main/java/io/nexusbot/modules/commands/ClearMessages.java
@@ -3,6 +3,7 @@ package io.nexusbot.modules.commands;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import io.github.r8zorg.jdatools.annotations.AdditionalSettings;
 import io.github.r8zorg.jdatools.annotations.Command;
@@ -19,22 +20,9 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 
 @SlashCommands
 public class ClearMessages {
-    @Command(description = "Удалить сообщения")
-    @AdditionalSettings(defaultPermissions = Permission.MESSAGE_MANAGE)
-    public void clear(SlashCommandInteractionEvent event,
-            @Option(name = "amount", description = "Количество. Не больше 100") Integer amount,
-            @Option(name = "member", description = "Участник, чьи сообщения нужно удалить", required = false) Member member,
-            // @Option(name = "regex", description = "Содержание сообщения должно
-            // соответствовать этому правилу", required = false) String regex,
-            // @Option(name = "everywhere", description = "Удалить сообщения во всех
-            // каналах", required = false) Boolean isEverywhere,
-            @Option(name = "channel", description = "Канал, в котором нужно удалить сообщения", channelType = ChannelType.TEXT, required = false) TextChannel channel) {
-        event.deferReply().setEphemeral(true).queue();
-
-        int messagesAmount = amount <= 100 ? amount : 100;
+    private void deleteMessages(SlashCommandInteractionEvent event, int messagesAmount, Member member,
+            MessageChannel messageChannel) {
         List<Message> messages = new ArrayList<>();
-        MessageChannel messageChannel = channel != null ? channel : event.getChannel();
-
         messageChannel.getIterableHistory()
                 .forEachAsync(message -> {
                     if (member == null) {
@@ -47,5 +35,21 @@ public class ClearMessages {
                 .thenRun(() -> messageChannel.purgeMessages(messages))
                 .thenRun(() -> EmbedUtil.replyEmbed(event.getHook(), "Удалено " + messages.size() + " сообщений",
                         Color.GREEN));
+    }
+
+    @Command(description = "Удалить сообщения")
+    @AdditionalSettings(defaultPermissions = Permission.MESSAGE_MANAGE)
+    public void clear(SlashCommandInteractionEvent event,
+            @Option(name = "amount", description = "Количество. Не больше 100") Integer amount,
+            @Option(name = "member", description = "Участник, чьи сообщения нужно удалить", required = false) Member member,
+            // @Option(name = "content", description = "Содержание сообщения должно соответствовать этому правилу", required = false) String content,
+            // @Option(name = "everywhere", description = "Удалить сообщения во всех каналах", required = false) Boolean isEverywhere,
+            @Option(name = "channel", description = "Канал, в котором нужно удалить сообщения", channelType = ChannelType.TEXT, required = false) TextChannel channel) {
+        event.deferReply().setEphemeral(true).queue();
+
+        int messagesAmount = amount <= 100 ? amount : 100;
+        MessageChannel messageChannel = channel != null ? channel : event.getChannel();
+        deleteMessages(event, messagesAmount, member, messageChannel);
+
     }
 }

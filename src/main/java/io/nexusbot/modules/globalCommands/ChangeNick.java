f1e696238c407bb7393c373228647eea7e7ff818
Hak1e <hakie.notiko@gmail.com>
2026-02-12 15:16:34 +0400

    Added catch HierachyException

--- src/main/java/io/nexusbot/modules/globalCommands/ChangeNick.java
+++ src/main/java/io/nexusbot/modules/globalCommands/ChangeNick.java
@@ -9,23 +9,30 @@ import io.github.r8zorg.jdatools.annotations.Subcommand;
 import io.nexusbot.utils.EmbedUtil;
 import io.nexusbot.utils.MembersUtil;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.exceptions.HierarchyException;
 
 @SlashCommands
 public class ChangeNick {
     @Command(description = "Изменить никнейм на сервере")
     public void nickname(SlashCommandInteractionEvent event) {
     }
-    @Subcommand(parentNames = "nickname",description = "Установить никнейм")
+
+    @Subcommand(parentNames = "nickname", description = "Установить никнейм")
     public void set(SlashCommandInteractionEvent event,
             @Option(name = "nick", description = "Новый никнейм") String nickname) {
         if (MembersUtil.inBlacklist(event.getUser().getIdLong())) {
             EmbedUtil.replyEmbed(event, "Вам запрещено испольовать эту команду", Color.RED);
             return;
         }
-        event.getMember().modifyNickname(nickname).queue(
-                success -> EmbedUtil.replyEmbed(event, "Ник успешно сменён", Color.GREEN),
-                error -> EmbedUtil.replyEmbed(event, "Возникла ошибка при смене никнейма: " + error.getMessage(),
-                        Color.RED));
+        try {
+            event.getMember().modifyNickname(nickname).queue(
+                    success -> EmbedUtil.replyEmbed(event, "Ник успешно сменён", Color.GREEN),
+                    error -> EmbedUtil.replyEmbed(event, "Возникла ошибка при смене никнейма: " + error.getMessage(),
+                            Color.RED));
+        } catch (HierarchyException e) {
+            EmbedUtil.replyEmbed(event, "Невозможно изменить никнейм участнику с ролью, равной или превышающей мою.",
+                    Color.RED);
+        }
     }
 
     @Subcommand(parentNames = "nickname", description = "Сбросить никнейм")

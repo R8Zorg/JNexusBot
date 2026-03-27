a7bdb26a6db3dc191816f290e488491393acaccc
Hak1e <hakie.notiko@gmail.com>
2026-03-27 17:08:11 +0400

    Removed unused methods

--- src/main/java/io/nexusbot/database/services/TempRoomSettingsService.java
+++ src/main/java/io/nexusbot/database/services/TempRoomSettingsService.java
@@ -1,10 +1,5 @@
 package io.nexusbot.database.services;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-import io.nexusbot.componentsData.ChannelOverrides;
 import io.nexusbot.database.dao.CrudDao;
 import io.nexusbot.database.entities.TempRoomSettings;
 import io.nexusbot.database.entities.TempRoomSettingsPK;
@@ -12,24 +7,6 @@ import io.nexusbot.database.entities.TempRoomSettingsPK;
 public class TempRoomSettingsService extends CrudDao<TempRoomSettings, TempRoomSettingsPK> {
     public TempRoomSettingsService() {
         super(TempRoomSettings.class);
-    }
-
-    public Map<Long, ChannelOverrides> getOverrides(TempRoomSettingsPK pk) {
-        TempRoomSettings settings = get(pk);
-        return settings != null
-                ? Collections.unmodifiableMap(settings.getOverrides())
-                : Collections.emptyMap();
-    }
-
-    public Map<Long, ChannelOverrides> getOverrides(long ownerId, long guildId) {
-        return getOverrides(new TempRoomSettingsPK(ownerId, guildId));
-    }
-
-    public void setOverrides(long ownerId, long guildId, HashMap<Long, ChannelOverrides> newOverrides) {
-        TempRoomSettingsPK pk = new TempRoomSettingsPK(ownerId, guildId);
-        TempRoomSettings tempRoomSettings = get(pk);
-        tempRoomSettings.setOverrides(newOverrides);
-        saveOrUpdate(tempRoomSettings);
     }
 
     public TempRoomSettings getOrCreate(long ownerId, long guildId) {

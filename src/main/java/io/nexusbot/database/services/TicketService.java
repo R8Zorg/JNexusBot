938aefe8e66fa6531e5a5f956732568e19f0b964
Hak1e <hakie.notiko@gmail.com>
2026-03-27 16:55:54 +0400

    Removed unused method

--- src/main/java/io/nexusbot/database/services/TicketService.java
+++ src/main/java/io/nexusbot/database/services/TicketService.java
@@ -7,12 +7,4 @@ public class TicketService extends AbstractCrudDao<Ticket, Long> {
     public TicketService() {
         super(Ticket.class);
     }
-
-    public Ticket getOrCreate(long guildId) {
-        Ticket ticket = get(guildId);
-        if (ticket == null) {
-            ticket = new Ticket(guildId);
-        }
-        return ticket;
-    }
 }

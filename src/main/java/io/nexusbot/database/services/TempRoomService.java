c09c6e526806b3acad14ced008f3c20025b86f39
Hak1e <hakie.notiko@gmail.com>
2026-03-28 09:18:37 +0400

    Removed useless method

--- src/main/java/io/nexusbot/database/services/TempRoomService.java
+++ src/main/java/io/nexusbot/database/services/TempRoomService.java
@@ -1,25 +1,10 @@
 package io.nexusbot.database.services;
 
-import java.util.function.Function;
-
 import io.nexusbot.database.dao.CrudDao;
 import io.nexusbot.database.entities.TempRoom;
 
 public class TempRoomService extends CrudDao<TempRoom, Long> {
     public TempRoomService() {
         super(TempRoom.class);
-    }
-
-    private <T> T extractField(long roomId, Function<TempRoom, T> extractor) {
-        TempRoom tempRoom = get(roomId);
-        return tempRoom == null ? null : extractor.apply(tempRoom);
-    }
-
-    public Long getOwnerId(long roomId) {
-        return extractField(roomId, TempRoom::getOwnerId);
-    }
-
-    public Long getCategoryId(long roomId) {
-        return extractField(roomId, TempRoom::getCategoryId);
     }
 }

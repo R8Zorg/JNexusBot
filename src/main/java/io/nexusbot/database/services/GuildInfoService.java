0b9fdbf68583342bc9e2b188fd09ac611b1cd00b
Hak1e <hakie.notiko@gmail.com>
2026-03-28 09:26:23 +0400

    Removed unused methods

--- src/main/java/io/nexusbot/database/services/GuildInfoService.java
+++ src/main/java/io/nexusbot/database/services/GuildInfoService.java
@@ -1,23 +1,10 @@
 package io.nexusbot.database.services;
 
-import java.util.NoSuchElementException;
-
-import org.hibernate.ObjectNotFoundException;
-
 import io.nexusbot.database.dao.CrudDao;
 import io.nexusbot.database.entities.GuildInfo;
 
 public class GuildInfoService extends CrudDao<GuildInfo, Long> {
-
     public GuildInfoService() {
         super(GuildInfo.class);
-    }
-
-    public long getOwnerId(long guildId) throws ObjectNotFoundException, NoSuchElementException {
-        GuildInfo guild = get(guildId);
-        if (guild == null) {
-            throw new NoSuchElementException("Guild with id " + guildId + " not found in database");
-        }
-        return guild.getOwnerId();
     }
 }

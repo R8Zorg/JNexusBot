d8b7a9ae58f3154300c841edf08a1e70087581e4
Hak1e <hakie.notiko@gmail.com>
2026-05-08 16:57:51 +0400

    Removed unused method

--- src/main/java/io/nexusbot/database/services/TempRoomCreatorService.java
+++ src/main/java/io/nexusbot/database/services/TempRoomCreatorService.java
@@ -1,22 +1,11 @@
 package io.nexusbot.database.services;
 
-import org.hibernate.Hibernate;
-
 import io.nexusbot.database.dao.CrudDao;
 import io.nexusbot.database.entities.TempRoomCreator;
 
 public class TempRoomCreatorService extends CrudDao<TempRoomCreator, Long> {
     public TempRoomCreatorService() {
         super(TempRoomCreator.class);
-    }
-
-    @Override
-    public TempRoomCreator get(Long channelCreatorId) {
-        TempRoomCreator tempRoomCreator = super.get(channelCreatorId);
-        if (tempRoomCreator != null) {
-            Hibernate.initialize(tempRoomCreator.getNeededRolesIds());
-        }
-        return tempRoomCreator;
     }
 
     public TempRoomCreator getOrCreate(long roomCreatorId) {

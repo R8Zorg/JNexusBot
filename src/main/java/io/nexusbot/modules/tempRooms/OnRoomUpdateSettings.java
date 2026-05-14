60f0694b0db1515bb29bec8c86c7b2c0edee131f
Hak1e <hakie.notiko@gmail.com>
2026-05-14 16:08:54 +0400

    Code refactoring. New feature: bot saves all channel overrides now

--- src/main/java/io/nexusbot/modules/tempRooms/OnRoomUpdateSettings.java
+++ src/main/java/io/nexusbot/modules/tempRooms/OnRoomUpdateSettings.java
@@ -1,14 +1,13 @@
 package io.nexusbot.modules.tempRooms;
 
+import java.util.Map;
 import java.util.function.BiConsumer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import io.github.r8zorg.jdatools.annotations.EventListeners;
 import io.nexusbot.database.entities.TempRoom;
 import io.nexusbot.database.services.TempRoomService;
 import io.nexusbot.utils.TempRoomUtil;
+import net.dv8tion.jda.api.entities.PermissionOverride;
 import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.events.channel.update.ChannelUpdateBitrateEvent;
 import net.dv8tion.jda.api.events.channel.update.ChannelUpdateNSFWEvent;
@@ -16,6 +15,7 @@ import net.dv8tion.jda.api.events.channel.update.ChannelUpdateNameEvent;
 import net.dv8tion.jda.api.events.channel.update.ChannelUpdateUserLimitEvent;
 import net.dv8tion.jda.api.events.channel.update.ChannelUpdateVoiceStatusEvent;
 import net.dv8tion.jda.api.events.channel.update.GenericChannelUpdateEvent;
+import net.dv8tion.jda.api.events.guild.override.GenericPermissionOverrideEvent;
 import net.dv8tion.jda.api.events.guild.override.PermissionOverrideCreateEvent;
 import net.dv8tion.jda.api.events.guild.override.PermissionOverrideDeleteEvent;
 import net.dv8tion.jda.api.events.guild.override.PermissionOverrideUpdateEvent;
@@ -23,8 +23,19 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 @EventListeners
 public class OnRoomUpdateSettings extends ListenerAdapter {
-    private final Logger LOGGER = LoggerFactory.getLogger(OnRoomUpdateSettings.class);
     private TempRoomService roomService = new TempRoomService();
+
+    private final Map<Class<?>, BiConsumer<VoiceChannel, Long>> CHANNEL_SETTINGS_HANDLERS = Map.of(
+            ChannelUpdateBitrateEvent.class, TempRoomUtil::saveBitrate,
+            ChannelUpdateNSFWEvent.class, TempRoomUtil::saveNsfw,
+            ChannelUpdateNameEvent.class, TempRoomUtil::saveName,
+            ChannelUpdateUserLimitEvent.class, TempRoomUtil::saveUserLimit,
+            ChannelUpdateVoiceStatusEvent.class, TempRoomUtil::saveVoiceStatus);
+
+    private final Map<Class<?>, BiConsumer<Long, PermissionOverride>> CHANNEL_OVERRIDES_HANDLERS = Map.of(
+            PermissionOverrideCreateEvent.class, TempRoomUtil::saveOverride,
+            PermissionOverrideDeleteEvent.class, TempRoomUtil::saveOverride,
+            PermissionOverrideUpdateEvent.class, TempRoomUtil::saveOverride);
 
     private void handleChannelUpdate(GenericChannelUpdateEvent<?> event, BiConsumer<VoiceChannel, Long> action) {
         TempRoom room = roomService.get(event.getChannel().getIdLong());
@@ -33,45 +44,27 @@ public class OnRoomUpdateSettings extends ListenerAdapter {
         }
     }
 
-    @Override
-    public void onChannelUpdateBitrate(ChannelUpdateBitrateEvent event) {
-        handleChannelUpdate(event, TempRoomUtil::saveBitrate);
+    private void handleChannelOverrideEdit(GenericPermissionOverrideEvent event,
+            BiConsumer<Long, PermissionOverride> action) {
+        TempRoom room = roomService.get(event.getChannel().getIdLong());
+        if (room != null) {
+            action.accept(room.getOwnerId(), event.getPermissionOverride());
+        }
     }
 
     @Override
-    public void onChannelUpdateNSFW(ChannelUpdateNSFWEvent event) {
-        handleChannelUpdate(event, TempRoomUtil::saveNsfw);
+    public void onGenericChannelUpdate(GenericChannelUpdateEvent<?> event) {
+        var consumer = CHANNEL_SETTINGS_HANDLERS.get((event.getClass()));
+        if (consumer != null) {
+            handleChannelUpdate(event, consumer);
+        }
     }
 
     @Override
-    public void onChannelUpdateName(ChannelUpdateNameEvent event) {
-        handleChannelUpdate(event, TempRoomUtil::saveName);
-    }
-
-    @Override
-    public void onChannelUpdateUserLimit(ChannelUpdateUserLimitEvent event) {
-        handleChannelUpdate(event, TempRoomUtil::saveUserLimit);
-    }
-
-    @Override
-    public void onChannelUpdateVoiceStatus(ChannelUpdateVoiceStatusEvent event) {
-        handleChannelUpdate(event, TempRoomUtil::saveVoiceStatus);
-    }
-
-    @Override
-    public void onPermissionOverrideCreate(PermissionOverrideCreateEvent event) {
-
-    }
-
-    @Override
-    public void onPermissionOverrideDelete(PermissionOverrideDeleteEvent event) {
-        // TODO Auto-generated method stub
-        super.onPermissionOverrideDelete(event);
-    }
-
-    @Override
-    public void onPermissionOverrideUpdate(PermissionOverrideUpdateEvent event) {
-        // TODO Auto-generated method stub
-        super.onPermissionOverrideUpdate(event);
+    public void onGenericPermissionOverride(GenericPermissionOverrideEvent event) {
+        var consumer = CHANNEL_OVERRIDES_HANDLERS.get(event.getClass());
+        if (consumer != null) {
+            handleChannelOverrideEdit(event, consumer);
+        }
     }
 }

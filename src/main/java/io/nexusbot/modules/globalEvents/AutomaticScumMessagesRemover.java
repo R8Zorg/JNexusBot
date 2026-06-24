ec1f3bbce150eade977bb6414312d6888d25c730
Hak1e <hakie.notiko@gmail.com>
2026-06-24 18:05:44 +0400

    Added send log after actions

--- src/main/java/io/nexusbot/modules/globalEvents/AutomaticScumMessagesRemover.java
+++ src/main/java/io/nexusbot/modules/globalEvents/AutomaticScumMessagesRemover.java
@@ -1,18 +1,25 @@
 package io.nexusbot.modules.globalEvents;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import io.github.r8zorg.jdatools.annotations.EventListeners;
 import io.nexusbot.database.entities.SpecialRoles;
+import io.nexusbot.database.entities.SpecialTextChannels;
 import io.nexusbot.database.services.SpecialRolesService;
+import io.nexusbot.database.services.SpecialTextChannelsService;
+import io.nexusbot.utils.EmbedUtil;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+// Если участник получил мьют, проверить все его следующие отправленные сообщения после этого
 
 record MessageInfo(long messageId, long channelId, String contentRaw) {
 }
@@ -22,6 +29,7 @@ public class AutomaticScumMessagesRemover extends ListenerAdapter {
     private final short messagesAmount = 3;
     private HashMap<Long, List<MessageInfo>> sentMessages = new HashMap<>();
     private SpecialRolesService specialRolesService = new SpecialRolesService();
+    private SpecialTextChannelsService specialTextChannelsService = new SpecialTextChannelsService();
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
@@ -40,6 +48,7 @@ public class AutomaticScumMessagesRemover extends ListenerAdapter {
                         receivedMessage.getContentRaw()));
                 if (memberMessages.size() >= messagesAmount) {
                     Guild guild = event.getGuild();
+                    String logMessage = event.getMember().getAsMention() + " помечен(а) за рассылку скама.\n";
 
                     SpecialRoles specialRoles = specialRolesService.get(guild.getIdLong());
                     if (specialRoles != null) {
@@ -48,15 +57,26 @@ public class AutomaticScumMessagesRemover extends ListenerAdapter {
                             Role muteRole = guild.getRoleById(muteRoleId);
                             if (muteRole != null) {
                                 guild.addRoleToMember(event.getMember(), muteRole).queue();
+                                logMessage += "И был замьючен.\n";
                             }
                         }
                     }
+                    logMessage += "Каналы, в которых удалены сообщения:\n";
                     for (MessageInfo messageInfo : memberMessages) {
                         GuildMessageChannel channel = guild.getChannelById(GuildMessageChannel.class,
                                 messageInfo.channelId());
                         channel.deleteMessageById(messageInfo.messageId()).queue();
+                        logMessage += channel.getAsMention() + "\n";
                     }
                     sentMessages.remove(userId);
+
+                    SpecialTextChannels specialTextChannels = specialTextChannelsService.get(guild.getIdLong());
+                    if (specialTextChannels != null) {
+                        TextChannel logChannel = guild.getTextChannelById(specialTextChannels.getTextLogChannelId());
+                        if (logChannel != null) {
+                            EmbedUtil.sendEmbed(logChannel, logMessage, Color.ORANGE);
+                        }
+                    }
                 }
             } else {
                 sentMessages.remove(userId);

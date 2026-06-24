320a15080ad215ddcbef537683284002a89b38c4
Hak1e <hakie.notiko@gmail.com>
2026-06-24 15:39:19 +0400

    Added command for setup mute role

---
+++ src/main/java/io/nexusbot/modules/setup/SetupRoles.java
@@ -0,0 +1,32 @@
+package io.nexusbot.modules.setup;
+
+import java.awt.Color;
+
+import io.github.r8zorg.jdatools.annotations.Option;
+import io.github.r8zorg.jdatools.annotations.SlashCommands;
+import io.github.r8zorg.jdatools.annotations.Subcommand;
+import io.github.r8zorg.jdatools.annotations.SubcommandGroup;
+import io.nexusbot.database.entities.SpecialRoles;
+import io.nexusbot.database.services.SpecialRolesService;
+import io.nexusbot.utils.EmbedUtil;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+@SlashCommands
+public class SetupRoles {
+    private SpecialRolesService specialRolesService = new SpecialRolesService();
+
+    @SubcommandGroup(parentName = "setup", description = "Группа команд для указания ролей")
+    public void role(SlashCommandInteractionEvent event) {
+    }
+
+    @Subcommand(parentNames = "setup role")
+    public void mute(SlashCommandInteractionEvent event,
+            @Option(name = "role", description = "Мьют роль") Role role) {
+            SpecialRoles specialRoles = new SpecialRoles(event.getGuild().getIdLong());
+            specialRoles.setMuteRoleId(role.getIdLong());
+            specialRolesService.saveOrUpdate(specialRoles);
+            EmbedUtil.replyEmbed(event, "Мьют-роль сохранена.", Color.GREEN);
+    }
+
+}

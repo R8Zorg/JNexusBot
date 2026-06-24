9c074f20194671cad7eb4f322d8efef872b23471
Hak1e <hakie.notiko@gmail.com>
2026-06-24 15:36:30 +0400

    SpecialRole -> SpecialRoles

---
+++ src/main/java/io/nexusbot/database/entities/SpecialRoles.java
@@ -0,0 +1,37 @@
+package io.nexusbot.database.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class SpecialRoles {
+    @Id
+    private long guildId;
+
+    private long muteRoleId;
+
+    public SpecialRoles() {
+    }
+
+    public SpecialRoles(long guildId) {
+        setGuildId(guildId);
+    }
+
+    public long getGuildId() {
+        return guildId;
+    }
+
+    public void setGuildId(long guildId) {
+        this.guildId = guildId;
+    }
+
+    public long getMuteRoleId() {
+        return muteRoleId;
+    }
+
+    public void setMuteRoleId(long mutedRoleId) {
+        this.muteRoleId = mutedRoleId;
+    }
+}

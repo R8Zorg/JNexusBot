b59e452b222be34158dd0def2f4a9320dc27919e
Hak1e <hakie.notiko@gmail.com>
2026-06-25 01:39:46 +0400

    Fixed overwrite bug

--- src/main/java/io/nexusbot/modules/setup/SetupChannels.java
+++ src/main/java/io/nexusbot/modules/setup/SetupChannels.java
@@ -9,6 +9,7 @@ import io.github.r8zorg.jdatools.annotations.SubcommandGroup;
 import io.nexusbot.database.entities.SpecialTextChannels;
 import io.nexusbot.database.services.SpecialTextChannelsService;
 import io.nexusbot.utils.EmbedUtil;
+import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -24,7 +25,7 @@ public class SetupChannels {
     @Subcommand(parentNames = "setup channel", description = "Канал для логов, связанных с сообщениями")
     public void text_log(SlashCommandInteractionEvent event,
             @Option(name = "channel", description = "Канал для текстовых логов", channelType = ChannelType.TEXT) TextChannel logChannel) {
-        SpecialTextChannels specialTextChannels = new SpecialTextChannels(event.getGuild().getIdLong());
+        SpecialTextChannels specialTextChannels = specialTextChannelsService.getOrCreate(event.getGuild().getIdLong());
         specialTextChannels.setTextLogChannelId(logChannel.getIdLong());
         specialTextChannelsService.saveOrUpdate(specialTextChannels);
         EmbedUtil.replyEmbed(event, "Сохранено.", Color.GREEN);
@@ -32,8 +33,8 @@ public class SetupChannels {
 
     @Subcommand(parentNames = "setup channel", description = "Канал для выдачи мьюта написавшего в него участника")
     public void mute(SlashCommandInteractionEvent event,
-            @Option(name = "channel", description = "Канал", channelType = ChannelType.TEXT) TextChannel channel) {
-        SpecialTextChannels specialTextChannels = new SpecialTextChannels(event.getGuild().getIdLong());
+            @Option(name = "channel", description = "Канал") Channel channel) {
+        SpecialTextChannels specialTextChannels = specialTextChannelsService.getOrCreate(event.getGuild().getIdLong());
         specialTextChannels.setMuteChannelId(channel.getIdLong());
         specialTextChannelsService.saveOrUpdate(specialTextChannels);
         EmbedUtil.replyEmbed(event, "Сохранено.", Color.GREEN);

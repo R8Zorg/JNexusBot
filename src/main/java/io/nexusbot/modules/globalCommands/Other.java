ee44e86e0eeae3d81bba3efe30f3f147a490c48c
Hak1e <hakie.notiko@gmail.com>
2026-06-26 22:25:43 +0400

    get_reacted_users -> get reacted users

--- src/main/java/io/nexusbot/modules/globalCommands/Other.java
+++ src/main/java/io/nexusbot/modules/globalCommands/Other.java
@@ -6,6 +6,8 @@ import java.util.List;
 import io.github.r8zorg.jdatools.annotations.Command;
 import io.github.r8zorg.jdatools.annotations.Option;
 import io.github.r8zorg.jdatools.annotations.SlashCommands;
+import io.github.r8zorg.jdatools.annotations.Subcommand;
+import io.github.r8zorg.jdatools.annotations.SubcommandGroup;
 import io.nexusbot.utils.EmbedUtil;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
@@ -15,8 +17,17 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 
 @SlashCommands
 public class Other {
+
     @Command(description = "Получить список всех участников, поставивших реакцию под сообщением")
-    public void get_reacted_users(SlashCommandInteractionEvent event,
+    public void get(SlashCommandInteractionEvent event) {
+    }
+
+    @SubcommandGroup(parentName = "get")
+    public void reacted(SlashCommandInteractionEvent event) {
+    }
+
+    @Subcommand(parentNames = "get reacted", description = "Получить список всех участников, поставивших реакцию под сообщением")
+    public void users(SlashCommandInteractionEvent event,
             @Option(name = "message_id", description = "Айди сообщения") String messageId,
             @Option(name = "channel", description = "Канал, в котором находится сообщение", channelType = ChannelType.TEXT) TextChannel textChannel) {
         StringBuilder info = new StringBuilder();

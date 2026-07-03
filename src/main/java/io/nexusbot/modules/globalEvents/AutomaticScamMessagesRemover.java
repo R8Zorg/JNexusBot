e9caf3d670b39c8470971aa7c891d6836ac43bdc
Hak1e <hakie.notiko@gmail.com>
2026-07-03 11:13:19 +0400

    Remove and exit if messages doesn't equal

--- src/main/java/io/nexusbot/modules/globalEvents/AutomaticScamMessagesRemover.java
+++ src/main/java/io/nexusbot/modules/globalEvents/AutomaticScamMessagesRemover.java
@@ -30,7 +30,6 @@ record MessageInfo(long channelId, String messageContent, OffsetDateTime message
 public class AutomaticScamMessagesRemover extends ListenerAdapter {
     private static final short MESSAGES_AMOUNT = 3;
     private static final int MESSAGES_HISTORY_POOL = 5;
-    // private static final long MINIMAL_MESSAGE_DELAY_MS = 600;
 
     private Map<Long, List<MessageInfo>> sentMessages = new ConcurrentHashMap<>();
     private SpecialRolesService specialRolesService = new SpecialRolesService();
@@ -71,13 +70,10 @@ public class AutomaticScamMessagesRemover extends ListenerAdapter {
             List<MessageInfo> messagesInfo = sentMessages.get(userId);
             MessageInfo lastMessageInfo = messagesInfo.getLast();
 
-            // long messageDelayMs = Duration.between(
-            //         lastMessageInfo.messageCreationTime(),
-            //         receivedMessage.getTimeCreated()).toMillis();
-            // if (messageDelayMs > MINIMAL_MESSAGE_DELAY_MS) {
-            //     sentMessages.remove(userId);
-            //     return;
-            // }
+            if (!lastMessageInfo.messageContent().equals(receivedContentRaw)) {
+                sentMessages.remove(userId);
+                return;
+            }
 
             if (lastMessageInfo.channelId() != receivedChannelId
                     && lastMessageInfo.messageContent().equals(receivedContentRaw)) {

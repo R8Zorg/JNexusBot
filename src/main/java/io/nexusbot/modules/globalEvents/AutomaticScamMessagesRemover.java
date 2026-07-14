f2d91bfd3ea43f6a412f797d2ee3bdccac7484a9
Hak1e <hakie.notiko@gmail.com>
2026-07-14 15:00:27 +0400

    Updated algorithm
    Important change: The code is designed to handle situations where a spam
    bot mimics a real user sending a message. In such cases, the delay
    between messages must be >= 600 milliseconds, during which time the bot
    has time to mute the member.

--- src/main/java/io/nexusbot/modules/globalEvents/AutomaticScamMessagesRemover.java
+++ src/main/java/io/nexusbot/modules/globalEvents/AutomaticScamMessagesRemover.java
@@ -2,10 +2,12 @@ package io.nexusbot.modules.globalEvents;
 
 import java.awt.Color;
 import java.time.OffsetDateTime;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import io.github.r8zorg.jdatools.annotations.EventListeners;
@@ -14,12 +16,12 @@ import io.nexusbot.database.entities.SpecialTextChannels;
 import io.nexusbot.database.services.SpecialRolesService;
 import io.nexusbot.database.services.SpecialTextChannelsService;
 import io.nexusbot.utils.EmbedUtil;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.Message.Attachment;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -29,7 +31,17 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  * Тогда можно будет сохранять отправленные сообщения вместо прохода по всем каналам.
 */
 
-record MessageInfo(long channelId, int messageContentHashcode, OffsetDateTime messageCreationTime) {
+class MessageInfo {
+    public final String messageSignature;
+    public final OffsetDateTime firstMessageTime;
+    public Set<Long> channelIds = new HashSet<>();
+    public int messagesCount = 1;
+
+    public MessageInfo(String messageSignature, OffsetDateTime firstMessageTime, long channelId) {
+        this.messageSignature = messageSignature;
+        this.firstMessageTime = firstMessageTime;
+        channelIds.add(channelId);
+    }
 }
 
 @EventListeners
@@ -37,37 +49,46 @@ public class AutomaticScamMessagesRemover extends ListenerAdapter {
     private static final short MESSAGES_AMOUNT = 3;
     private static final int MESSAGES_HISTORY_POOL = 5;
 
-    private Map<Long, List<MessageInfo>> sentMessages = new ConcurrentHashMap<>();
     private SpecialRolesService specialRolesService = new SpecialRolesService();
     private SpecialTextChannelsService specialTextChannelsService = new SpecialTextChannelsService();
 
-    private void deleteMessages(MessageChannel channel, long memberId, int messageHashcode,
-            OffsetDateTime firstMessageCreationTime) {
-        channel.getHistory()
-                .retrievePast(MESSAGES_HISTORY_POOL)
-                .queue(history -> {
-                    history.stream()
-                            .filter(message -> message.getAuthor().getIdLong() == memberId)
-                            .filter(message -> message.getTimeCreated().isAfter(firstMessageCreationTime)
-                                    || message.getTimeCreated().isEqual(firstMessageCreationTime))
-                            .filter(message -> getMessageHashcode(message) == messageHashcode)
-                            .forEach(message -> channel.deleteMessageById(message.getIdLong()).queue());
-                });
+    private Map<Long, MessageInfo> sentMessages = new ConcurrentHashMap<>();
+
+    private void deleteMessages(Guild guild, long memberId, MessageInfo messageInfo) {
+        List<GuildMessageChannel> channels = messageInfo.channelIds.stream()
+                .map(id -> guild.getChannelById(GuildMessageChannel.class, id))
+                .filter(Objects::nonNull)
+                .toList();
+        for (MessageChannel channel : channels) {
+            channel.getHistory()
+                    .retrievePast(MESSAGES_HISTORY_POOL)
+                    .queue(history -> {
+                        List<Message> messages = history.stream()
+                                .filter(message -> message.getAuthor().getIdLong() == memberId)
+                                .filter(message -> message.getTimeCreated().isAfter(messageInfo.firstMessageTime)
+                                        || message.getTimeCreated().isEqual(messageInfo.firstMessageTime))
+                                .filter(message -> getMessageSignature(message) == messageInfo.messageSignature)
+                                .toList();
+                        if (!messages.isEmpty()) {
+                            channel.purgeMessages(messages);
+                        }
+                    });
+        }
     }
 
-    private int getMessageHashcode(Message message) {
-        String messageContent = message.getContentRaw();
+    private String getMessageSignature(Message message) {
+        StringBuilder messageContent = new StringBuilder(message.getContentRaw());
         List<Attachment> attachments = message.getAttachments();
         for (Attachment attachment : attachments) {
-            messageContent += String.format(
+            messageContent.append(String.format(
                     "|%s|%d|%s|%d|%d",
                     attachment.getFileName(),
                     attachment.getSize(),
                     attachment.getContentType(),
                     attachment.getWidth(),
-                    attachment.getHeight());
+                    attachment.getHeight()));
         }
-        return messageContent.hashCode();
+        return messageContent.toString();
 
     }
 
@@ -78,74 +99,57 @@ public class AutomaticScamMessagesRemover extends ListenerAdapter {
         }
 
         long userId = event.getAuthor().getIdLong();
-        Message receivedMessage = event.getMessage();
-        long receivedChannelId = event.getChannel().getIdLong();
-        int receivedMessageHashcode = getMessageHashcode(receivedMessage);
+        Message message = event.getMessage();
+        long channelId = event.getChannel().getIdLong();
+        String messageSignature = getMessageSignature(message);
 
-        MessageInfo messageInfo = new MessageInfo(receivedChannelId, receivedMessageHashcode,
-                receivedMessage.getTimeCreated());
+        MessageInfo messageInfo = sentMessages.get(userId);
 
-        if (sentMessages.get(userId) == null) {
-            sentMessages.put(userId, new ArrayList<MessageInfo>(
-                    List.of(messageInfo)));
-        } else {
-            List<MessageInfo> messagesInfo = sentMessages.get(userId);
-            MessageInfo lastMessageInfo = messagesInfo.getLast();
+        if (messageInfo == null) {
+            sentMessages.put(userId, new MessageInfo(messageSignature, message.getTimeCreated(), channelId));
+            return;
+        }
 
-            if (lastMessageInfo.messageContentHashcode() != receivedMessageHashcode) {
-                sentMessages.remove(userId);
-                return;
+        if (messageInfo.channelIds.contains(channelId)
+                || !messageInfo.messageSignature.equals(messageSignature)) {
+            sentMessages.remove(userId);
+            return;
+        }
+
+        messageInfo.channelIds.add(channelId);
+        messageInfo.messagesCount += 1;
+        if (messageInfo.messagesCount >= MESSAGES_AMOUNT) {
+            Guild guild = event.getGuild();
+            StringBuilder logMessage = new StringBuilder(
+                    event.getAuthor().getAsMention() + " помечается за рассылку скам сообщений");
+
+            SpecialRoles specialRoles = specialRolesService.get(guild.getIdLong());
+
+            Role muteRole = Optional.ofNullable(specialRoles)
+                    .map(SpecialRoles::getMuteRoleId)
+                    .map(guild::getRoleById)
+                    .orElse(null);
+
+            if (muteRole != null) {
+                guild.addRoleToMember(event.getAuthor(), muteRole).queue();
+                logMessage.append(" и получает мьют");
+                // TODO: отправить сообщение в чат замьюченных, на котором можно нажать кнопку
+                // для снятия роли, подтверждая смену пароля
             }
 
-            if (lastMessageInfo.channelId() != receivedChannelId) {
-                messagesInfo.add(messageInfo);
-                if (messagesInfo.size() >= MESSAGES_AMOUNT) {
-                    Guild guild = event.getGuild();
-                    String logMessage = event.getAuthor().getAsMention() + " помечается за рассылку скам сообщений";
+            logMessage.append(".");
 
-                    SpecialRoles specialRoles = specialRolesService.get(guild.getIdLong());
-                    if (specialRoles != null) {
-                        Long muteRoleId = specialRoles.getMuteRoleId();
-                        if (muteRoleId != null) {
-                            Role muteRole = guild.getRoleById(muteRoleId);
-                            if (muteRole != null) {
-                                guild.addRoleToMember(event.getAuthor(), muteRole).queue();
-                                logMessage += " и получает мьют";
-                                // TODO: отправить сообщение в чат замьюченных, на котором можно нажать кнопку
-                                // для снятия роли, подтверждая смену пароля
-                            }
-                        }
-                    }
-                    logMessage += ".";
+            deleteMessages(guild, userId, messageInfo);
+            sentMessages.remove(userId);
 
-                    Role targetRole = Optional.ofNullable(specialRoles)
-                            .map(SpecialRoles::getDefaultRoleId)
-                            .map(guild::getRoleById)
-                            .orElse(guild.getPublicRole());
+            TextChannel logChannel = Optional.ofNullable(specialTextChannelsService.get(guild.getIdLong()))
+                    .map(SpecialTextChannels::getTextLogChannelId)
+                    .map(guild::getTextChannelById)
+                    .orElse(null);
 
-                    List<MessageChannel> channels = guild.getChannels().stream()
-                            .filter(_channel -> _channel instanceof MessageChannel)
-                            .filter(_channel -> targetRole.hasPermission(_channel, Permission.MESSAGE_SEND))
-                            .map(_channel -> (MessageChannel) _channel)
-                            .toList();
-                    OffsetDateTime firstMessageSentTime = messagesInfo.getFirst().messageCreationTime();
-                    for (MessageChannel _channel : channels) {
-                        deleteMessages(_channel, userId, receivedMessageHashcode, firstMessageSentTime);
-                    }
-                    sentMessages.remove(userId);
-
-                    SpecialTextChannels specialTextChannels = specialTextChannelsService.get(guild.getIdLong());
-                    if (specialTextChannels != null) {
-                        TextChannel logChannel = guild.getTextChannelById(specialTextChannels.getTextLogChannelId());
-                        if (logChannel != null) {
-                            EmbedUtil.sendEmbed(logChannel, logMessage, Color.ORANGE);
-                        }
-                    }
-                }
-            } else {
-                sentMessages.remove(userId);
+            if (logChannel != null) {
+                EmbedUtil.sendEmbed(logChannel, logMessage.toString(), Color.ORANGE);
             }
         }
     }
-
 }

7c34567b16e3d636535437886cc921ee9db8a95d
Hak1e <hakie.notiko@gmail.com>
2026-07-14 16:16:55 +0400

    Added Caffeine for auto deleting records. Upgraded logic
    Added field "targeted". This prevents a situation where a member
    manages to send  more than 3 messages before being muted, and the bot
    does not delete those messages. This also helps remove all scam messages
    in case the mute role was not assigned for some reason.

--- src/main/java/io/nexusbot/modules/globalEvents/AutomaticScamMessagesRemover.java
+++ src/main/java/io/nexusbot/modules/globalEvents/AutomaticScamMessagesRemover.java
@@ -1,6 +1,7 @@
 package io.nexusbot.modules.globalEvents;
 
 import java.awt.Color;
+import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.util.HashSet;
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 
 import io.github.r8zorg.jdatools.annotations.EventListeners;
 import io.nexusbot.database.entities.SpecialRoles;
@@ -26,16 +30,12 @@ import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-/* TODO: логировать время между отправленными сообщениями.
- * Если они имитируют обычного участника, бот успеет выдать мьют до отправки 4го сообщения.
- * Тогда можно будет сохранять отправленные сообщения вместо прохода по всем каналам.
-*/
-
 class MessageInfo {
     public final String messageSignature;
     public final OffsetDateTime firstMessageTime;
     public Set<Long> channelIds = new HashSet<>();
     public int messagesCount = 1;
+    public boolean targeted = false;
 
     public MessageInfo(String messageSignature, OffsetDateTime firstMessageTime, long channelId) {
         this.messageSignature = messageSignature;
@@ -48,11 +48,13 @@ class MessageInfo {
 public class AutomaticScamMessagesRemover extends ListenerAdapter {
     private static final short MESSAGES_AMOUNT = 3;
     private static final int MESSAGES_HISTORY_POOL = 5;
+    private static final int CACHE_INFO_LIVE_MINUTES = 3;
 
     private SpecialRolesService specialRolesService = new SpecialRolesService();
     private SpecialTextChannelsService specialTextChannelsService = new SpecialTextChannelsService();
 
-    private Map<Long, MessageInfo> sentMessages = new ConcurrentHashMap<>();
+    private Cache<Long, MessageInfo> sentMessages = Caffeine.newBuilder()
+            .expireAfterWrite(Duration.ofMinutes(CACHE_INFO_LIVE_MINUTES)).build();
 
     private void deleteMessages(Guild guild, long memberId, MessageInfo messageInfo) {
         List<GuildMessageChannel> channels = messageInfo.channelIds.stream()
@@ -67,7 +69,7 @@ public class AutomaticScamMessagesRemover extends ListenerAdapter {
                                 .filter(message -> message.getAuthor().getIdLong() == memberId)
                                 .filter(message -> message.getTimeCreated().isAfter(messageInfo.firstMessageTime)
                                         || message.getTimeCreated().isEqual(messageInfo.firstMessageTime))
-                                .filter(message -> getMessageSignature(message) == messageInfo.messageSignature)
+                                .filter(message -> getMessageSignature(message).equals(messageInfo.messageSignature))
                                 .toList();
                         if (!messages.isEmpty()) {
                             channel.purgeMessages(messages);
@@ -92,6 +94,10 @@ public class AutomaticScamMessagesRemover extends ListenerAdapter {
 
     }
 
+    private boolean isMessagesEqual(MessageInfo messageInfo, String messageSignature) {
+        return messageInfo.messageSignature.equals(messageSignature);
+    }
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getAuthor().isBot()) {
@@ -103,26 +109,34 @@ public class AutomaticScamMessagesRemover extends ListenerAdapter {
         long channelId = event.getChannel().getIdLong();
         String messageSignature = getMessageSignature(message);
 
-        MessageInfo messageInfo = sentMessages.get(userId);
+        MessageInfo messageInfo = sentMessages.getIfPresent(userId);
 
         if (messageInfo == null) {
             sentMessages.put(userId, new MessageInfo(messageSignature, message.getTimeCreated(), channelId));
             return;
         }
 
+        if (messageInfo.targeted
+                && isMessagesEqual(messageInfo, messageSignature)) {
+            event.getChannel().deleteMessageById(message.getIdLong()).queue();
+            return;
+        }
+
         if (messageInfo.channelIds.contains(channelId)
-                || !messageInfo.messageSignature.equals(messageSignature)) {
-            sentMessages.remove(userId);
+                || !isMessagesEqual(messageInfo, messageSignature)) {
+            sentMessages.invalidate(userId);
             return;
         }
 
         messageInfo.channelIds.add(channelId);
         messageInfo.messagesCount += 1;
+
         if (messageInfo.messagesCount >= MESSAGES_AMOUNT) {
-            Guild guild = event.getGuild();
+            messageInfo.targeted = true;
             StringBuilder logMessage = new StringBuilder(
                     event.getAuthor().getAsMention() + " помечается за рассылку скам сообщений");
 
+            Guild guild = event.getGuild();
             SpecialRoles specialRoles = specialRolesService.get(guild.getIdLong());
 
             Role muteRole = Optional.ofNullable(specialRoles)
@@ -140,7 +154,6 @@ public class AutomaticScamMessagesRemover extends ListenerAdapter {
             logMessage.append(".");
 
             deleteMessages(guild, userId, messageInfo);
-            sentMessages.remove(userId);
 
             TextChannel logChannel = Optional.ofNullable(specialTextChannelsService.get(guild.getIdLong()))
                     .map(SpecialTextChannels::getTextLogChannelId)
@@ -150,6 +163,7 @@ public class AutomaticScamMessagesRemover extends ListenerAdapter {
             if (logChannel != null) {
                 EmbedUtil.sendEmbed(logChannel, logMessage.toString(), Color.ORANGE);
             }
+            return;
         }
     }
 }
